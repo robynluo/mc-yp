972cecd8930f8ab655c2e5a1d6b589377d3569af
robyn <binbin520898@163.com>
2016-06-15 21:05:04 +0800

    demo

---
+++ bosheng/js/base.js
@@ -0,0 +1,15 @@
+$(function(){ 
+	$("#nav li").click(function(){
+	 var me=$(this);
+	  me.addClass("active").siblings("li").removeClass("active");
+	});
+
+
+	/*滚动页面隐藏或者显示*/
+	$(window).scroll(function(){
+        var topNav = $(".top-nav");
+
+		$(this).scrollTop()>100?topNav.stop(false,true).addClass("active"):topNav.stop(false,true).removeClass("active");
+	});
+});
+
